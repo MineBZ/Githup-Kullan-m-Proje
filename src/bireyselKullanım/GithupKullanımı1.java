7df620464f9126156004e423f9e28b1b550dbbc6
Mine Boz <bozmine574@gmail.com>
2022-05-15 21:14:42 +0300

    ilk değişiklik

--- src/bireyselKullanım/GithupKullanımı1.java
+++ src/bireyselKullanım/GithupKullanımı1.java
@@ -3,6 +3,9 @@ package bireyselKullanım;
 public class GithupKullanımı1 {
     public static void main(String[] args) {
         System.out.println("hello world");
+
+        System.out.println("that'is it");
+
     }
 
 }
